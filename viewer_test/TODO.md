02d946e73cd25b8cc7e0838efb099bc4debf2324
Chaitya Shah <chaityacshah@gmail.com>
2018-08-24 12:59:33 -0400

    config = config_messy and add TODO

---
+++ viewer_test/TODO.md
@@ -0,0 +1,13 @@
+Notes and to-do list for the developers of this tool:
+
+- config.py failed to work (incompatibility issues) with dendro_helpers.py after sorting functionality was added. Saving `config_messy.py` as `config.py`.
+- commit `029055c` (Aug. 10) has the working config.py without sorting.
+
+TODO:
+
+- Currently, some default color comes up after making initial plot (or after changing a parameter). Need to change it to black.
+- Need to add GUI option for sort by `parameter`.
+- Need to store depth/height of the tree. `depth` be used later.
+- Test the features integrated with data factory.
+- Test if data factory works for strings where no branch size is provided.
+
--- viewer_test/config.py
+++ viewer_test/config.py
@@ -3,6 +3,8 @@ import os
 import numpy as np
 
 from matplotlib.collections import LineCollection
+import matplotlib.cm as cm
+import matplotlib.colors as mplcolors
 
 from qtpy.QtWidgets import QWidget, QVBoxLayout, QCheckBox, QButtonGroup, QRadioButton, QHBoxLayout
 
@@ -11,15 +13,16 @@ from glue.core.data_combo_helper import ComponentIDComboHelper
 
 from glue.external.echo import CallbackProperty, SelectionCallbackProperty
 from glue.external.echo.qt import (connect_checkable_button,
-                                   autoconnect_callbacks_to_qt)
+                                   autoconnect_callbacks_to_qt,
+                                   connect_value)
 
 from glue.viewers.matplotlib.layer_artist import MatplotlibLayerArtist
 from glue.viewers.matplotlib.state import MatplotlibDataViewerState, MatplotlibLayerState
 from glue.viewers.matplotlib.qt.data_viewer import MatplotlibDataViewer
 
-from glue.utils.qt import load_ui
+from glue.utils.qt import load_ui, fix_tab_widget_fontsize
 
-from dendro_helpers import dendro_layout
+from dendro_helpers import dendro_layout, calculate_nleaf, sort1Darrays
 
 
 class TutorialViewerState(MatplotlibDataViewerState):
@@ -37,7 +40,7 @@ class TutorialViewerState(MatplotlibDataViewerState):
         self.add_callback('layers', self._on_layers_change)
         self.add_callback('x_att', self._on_attribute_change)
         self.add_callback('y_att', self._on_attribute_change)
-        TutorialViewerState.orientation.set_choices(self, ['horizontal', 'vertical', 'left', 'right'])
+        TutorialViewerState.orientation.set_choices(self, ['bottom-up', 'left-right', 'top-down', 'right-left'])
         self.add_callback('orientation', self._on_attribute_change)
 
 
@@ -48,16 +51,16 @@ class TutorialViewerState(MatplotlibDataViewerState):
     def _on_attribute_change(self, value):
         if self.y_att is not None:
 
-            if self.orientation == 'vertical':
+            if (self.orientation == 'bottom-up') or (self.orientation == 'top-down'):
                 self.x_axislabel = ''
                 self.y_axislabel = self.y_att.label
-            elif self.orientation == 'horizontal':
+            elif (self.orientation == 'left-right') or (self.orientation == 'right-left'):
                 self.x_axislabel = self.y_att.label
                 self.y_axislabel = ''
 
 
 class TutorialLayerState(MatplotlibLayerState):
-    fill = CallbackProperty(False, docstring='Whether to show the markers as filled or not')
+    linewidth = CallbackProperty(1, docstring='line width')
 
 
 class TutorialLayerArtist(MatplotlibLayerArtist):
@@ -73,11 +76,11 @@ class TutorialLayerArtist(MatplotlibLayerArtist):
         self.artist = self.axes.add_collection(self.lc)
         self.mpl_artists.append(self.artist)
 
-        self.state.add_callback('fill', self._on_visual_change)
         self.state.add_callback('visible', self._on_visual_change)
         self.state.add_callback('zorder', self._on_visual_change)
         self.state.add_callback('color', self._on_visual_change)
         self.state.add_callback('alpha', self._on_visual_change)
+        self.state.add_callback('linewidth', self._on_visual_change)
 
         self._viewer_state.add_callback('x_att', self._on_attribute_change)
         self._viewer_state.add_callback('y_att', self._on_attribute_change)
@@ -88,6 +91,7 @@ class TutorialLayerArtist(MatplotlibLayerArtist):
         self.artist.set_visible(self.state.visible)
         self.artist.set_zorder(self.state.zorder)
         self.lc.set_color(self.state.color)
+        self.lc.set_linewidth(self.state.linewidth)
         #self.artist.set_markeredgecolor(self.state.color)
         # if self.state.fill:
         #     self.artist.set_markerfacecolor(self.state.color)
@@ -102,25 +106,71 @@ class TutorialLayerArtist(MatplotlibLayerArtist):
         if self._viewer_state.x_att is None or self._viewer_state.y_att is None:
             return
 
+        #parent
         x = self.state.layer[self._viewer_state.x_att]
+        #height
         y = self.state.layer[self._viewer_state.y_att]
 
-        ###
         orientation = self._viewer_state.orientation
 
-        verts = dendro_layout(x, y, orientation = orientation)
 
+        ### sorty by
+        ##### sorby_array = None for using the orignal order
+        sortby_array = y ### sort by height
+        x, y = sort1Darrays(x, y, sortby_array)
+
+        verts, verts_horiz = dendro_layout(x, y, orientation = orientation)
+        nleaf = calculate_nleaf(x)
+
+
+        ###  Fix the input!
+        color_code = 'linear'
+        color_code_by = y  ## height
+        color_code_cmap = cm.Reds
+
+        ####
+        if color_code == 'fixed':
+
+            verts_final = np.concatenate([verts, verts_horiz])
+            colors_final = list(np.ones(len(verts_final)))
+
+        elif color_code == 'linear':
+
+            cmap = color_code_cmap
+            normalize = mplcolors.Normalize(np.nanmin(y), np.nanmax(y))
+
+            colors = [cmap(normalize(yi)) for yi in y]
+            colors_horiz = []
+            for i in range(len(verts_horiz)):
+                colors_horiz.append((0., 0., 0., 1.))
+
+            verts_final = np.concatenate([verts, verts_horiz])
+
+            colors_final = np.concatenate([colors, colors_horiz])
 
         #self.artist.set_data(x, y)
-        self.lc.set_segments(verts)
+        self.lc.set_segments(verts_final)
+        self.lc.set_color(colors_final)
 
+        # parent
+        xmin = (-.5)
+        xmax = nleaf+1.5
+        # height
+        ymin = np.nanmin(y)-.05*(np.nanmax(y)-np.nanmin(y))
+        ymax = np.nanmax(y)+.05*(np.nanmax(y)-np.nanmin(y))
 
-        if orientation == 'vertical':
-            self.axes.set_xlim(np.nanmin(x), np.nanmax(x))
-            self.axes.set_ylim(np.nanmin(y), np.nanmax(y))
-        elif orientation == 'horizontal':
-            self.axes.set_ylim(np.nanmin(x), np.nanmax(x))
-            self.axes.set_xlim(np.nanmin(y), np.nanmax(y))
+        if orientation == 'bottom-up':
+            self.axes.set_xlim(xmin, xmax)
+            self.axes.set_ylim(ymin, ymax)
+        elif orientation == 'top-down':
+            self.axes.set_xlim(xmin, xmax)
+            self.axes.set_ylim(ymax, ymin)
+        elif orientation == 'left-right':
+            self.axes.set_ylim(xmin, xmax)
+            self.axes.set_xlim(ymin, ymax)
+        elif orientation == 'right-left':
+            self.axes.set_ylim(xmin, xmax)
+            self.axes.set_xlim(ymax, ymin)
 
         self.redraw()
 
@@ -175,7 +225,6 @@ class TutorialLayerStateWidget(QWidget):
         self.setLayout(layout)
 
         self.layer_state = layer_artist.state
-        connect_checkable_button(self.layer_state, 'fill', self.checkbox)
 
 
 class TutorialLayerStyleEditor(QWidget):
@@ -190,6 +239,11 @@ class TutorialLayerStyleEditor(QWidget):
         connect_kwargs = {'alpha': dict(value_range=(0, 1))}
 
         autoconnect_callbacks_to_qt(layer.state, self.ui, connect_kwargs)
+
+
+
+
+
 
 
 
@@ -228,6 +282,7 @@ class TutorialDataViewer(MatplotlibDataViewer):
     _subset_artist_cls = TutorialLayerArtist
     _tool = MyCustomButton
     _layer_style_widget_cls = TutorialLayerStyleEditor
+    # _layer_style_widget_cls = ScatterLayerStyleEditor
 
     #####
     tools = ['select:rectangle']
